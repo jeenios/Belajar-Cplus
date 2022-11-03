5194e66dd921fc053bccf13f223d33ebe8dc30ad
unknown <alilatukau07@gmail.com>
2022-11-03 17:58:00 +0800

    Deklarasi

---
+++ 03_Deklarasi/deklarasi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a;
+    a = 10;
+    cout << a << endl;
+
+    int b;
+    cout << "MASUKAN NILAI ANDA : ";
+    cin >> b;
+    cout << "NILAI ANDA ADALAH : ";
+    cout << b;
+    cout << " TERIMAKASIH";
+
+    cin.get();
+    return 0;
+}
